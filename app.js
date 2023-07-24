17ea1c37a20a868ff2d86578c5200c698e19c2b2
chenchaiah98 <rchenchaiah98@gmail.com>
2023-07-24 03:28:50 +0000

    api code change

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const dateFunction = require("date-fns/addDays");
+const dateAdds = require("date-fns/addDays");
 
-const date = dateFunction(new Date(), 100);
-// DD/MM/YYYY
-console.log(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`);
-// module.exports = date;
+app.get("/", (request, response) => {
+  const todayDate = dateAdds(new Date(), 100);
+  response.send(
+    `${todayDate.getDate()}/${
+      todayDate.getMonth() + 1
+    }/${todayDate.getFullYear()}`
+  );
+});
+
+module.exports = app;
